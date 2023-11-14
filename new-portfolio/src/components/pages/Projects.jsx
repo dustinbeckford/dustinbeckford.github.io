f79a7e7f6de8e2a3a0db61c2cd7986bfd098d255
Dustin Beckford <dustin.beckford@gmail.com>
2023-11-14 12:19:52 -0500

    almost ready for deployment

--- new-portfolio/src/components/pages/Projects.jsx
+++ new-portfolio/src/components/pages/Projects.jsx
@@ -13,6 +13,7 @@ const Projects = () => {
 			description:
 				"This project is a Car Search Engine that uses the 'Cars API by Api Ninjas' to provide users with detailed vehicle specifications. Users can input Make, Model, and Year details to retrieve key information about a specific vehicle. The application dynamically displays these details on the webpage, including Fuel Type, Cylinders, Transmission type, and MPG values for City, Highway, and Combined driving. The color scheme is modern with white and gray shades, and the inclusion of photos enhances the overall user experience. This project aims to assist users in making informed decisions when considering the purchase of a vehicle.",
 			link: "https://github.com/dustinbeckford/front-end-project",
+			demoLink: "https://youtu.be/4vduEZJy_Sc",
 			image: CarSearchImage,
 		},
 		{
@@ -20,6 +21,7 @@ const Projects = () => {
 			description:
 				"The CarEvent App is an application designed to simplify the process of hosting and managing car events. It provides a user-friendly interface and comprehensive features for event organizers to track participants, their cars, event details, and sponsorships.",
 			link: "https://github.com/dustinbeckford/backend-project-2",
+			demoLink: "https://youtu.be/il2XWkDi5r4",
 			image: CarEventImage,
 		},
 		{
@@ -27,6 +29,7 @@ const Projects = () => {
 			description:
 				"For our Capstone final project we chose to create a gaming social application utilizing our full-stack web development skills in an agile fashion in two weeks' time. This application is designed to provide gamers with a platform to connect, communicate, and game together. Whether you're looking for new friends to play with, organizing gaming sessions, or simply chatting about your favorite games, our app has you covered.",
 			link: "https://github.com/August-2023-Capstone/Capstone/tree/main",
+			demoLink: "https://youtu.be/UPQ1Q1FtLTs",
 			image: GamerNetImage,
 		},
 	];
