5f0aecf791c78c53275ed40253db824686c2fb65
Dustin Beckford <dustin.beckford@gmail.com>
2025-10-17 13:12:03 -0400

    Enhance mobile responsiveness across the portfolio by adding media queries for layout adjustments, font sizes, and mobile menu toggle functionality in Navbar component.

--- new-portfolio/src/components/Navbar.jsx
+++ new-portfolio/src/components/Navbar.jsx
@@ -1,38 +1,57 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import Navlogo from "../assets/logo/8c3486cee545bfba69d20c3459509c51.jpg";
 import "../assets/CSS/Navbar.css";
 
 export const NavBar = () => {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	const toggleMobileMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen);
+	};
+
+	const closeMobileMenu = () => {
+		setIsMobileMenuOpen(false);
+	};
+
 	return (
 		<section>
 			<div className='Navbar-bigbox'>
 				<nav className='navbar'>
-					<Link to='/'>
+					<Link to='/' onClick={closeMobileMenu}>
 						<img className='navlogo' src={Navlogo} alt='Logo' />
 					</Link>
 
-					<ul className='nav-links'>
+					<div 
+						className={`mobile-menu-toggle ${isMobileMenuOpen ? 'active' : ''}`}
+						onClick={toggleMobileMenu}
+					>
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+
+					<ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
 						<li>
-							<ScrollLink to='Welcome' smooth={true} duration={500}>
+							<ScrollLink to='Welcome' smooth={true} duration={500} onClick={closeMobileMenu}>
 								Welcome
 							</ScrollLink>
 						</li>
 						<li>
-							<ScrollLink to='About' smooth={true} duration={500}>
+							<ScrollLink to='About' smooth={true} duration={500} onClick={closeMobileMenu}>
 								About
 							</ScrollLink>
 						</li>
 						<li>
-							<ScrollLink to='Projects' smooth={true} duration={500}>
+							<ScrollLink to='Projects' smooth={true} duration={500} onClick={closeMobileMenu}>
 								Projects
 							</ScrollLink>
 						</li>
 						<li>
-							<ScrollLink to='Contact' smooth={true} duration={500}>
+							<ScrollLink to='Contact' smooth={true} duration={500} onClick={closeMobileMenu}>
 								Contact
 							</ScrollLink>
 						</li>
